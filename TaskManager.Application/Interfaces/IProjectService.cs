6c46360e6e5369e38bf1bbffd90b230463595d4b
Carlos Marques <carlos.marques89@gmail.com>
2025-07-12 13:43:40 -0400

    Adding interface service

---
+++ TaskManager.Application/Interfaces/IProjectService.cs
@@ -0,0 +1,16 @@
+﻿using TaskManager.Domain;
+using static TaskManager.Domain.Enum;
+
+namespace TaskManager.Application.Interfaces;
+
+public interface IProjectService
+{
+    Task<Guid> CriarProjetoAsync(string nome);
+    Task<IEnumerable<Project>> ListarProjetosAsync();
+    Task<Project?> ObterProjetoAsync(Guid id);
+    Task<Guid> AdicionarTarefaAsync(Guid projetoId, string titulo, string descricao, DateTime vencimento, TaskPriority prioridade);
+    Task AtualizarTarefaAsync(Guid tarefaId, string? titulo, string? descricao, Domain.Enum.TaskStatus? status, string usuario);
+    Task RemoverTarefaAsync(Guid tarefaId);
+    Task RemoverProjetoAsync(Guid projetoId);
+    Task AdicionarComentarioAsync(Guid tarefaId, string texto, string usuario);
+}
